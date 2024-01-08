7c114a5ae0f7b56b9cd644b3e9f57751a0926ffb
kajtekk <slowko.kajetan@gmail.com>
2024-01-08 23:30:23 +0100

    added the handling of edge cases when the end is not a size of full double mask

--- source/SmartSignalFilter/SmartSignalFilter.cpp
+++ source/SmartSignalFilter/SmartSignalFilter.cpp
@@ -114,7 +114,45 @@ void SmartSignalFilter::filter(std::shared_ptr<Signal> newPartOfSignal, int firs
                 //endOfPrefixMask = signalSize - 1;
                 // when the prefix(postfix in real) mask ends after the size of our signal,
                 // it would be the best to implement approach from naive solution, coz we cant even calc
-                // one mask at this point
+                // one full mask at this point
+                //zle to jest(usuniete), trzeba to rozwiazac tak jak naive, ale biorac pod uwage to, ze startOfPrefixMask to nie jest srodek maski, a poczatek,
+                //wiec tak naprawde srodkiem bedzie startOfPrefixMask + maskOneHalfLength
+                int indexOfTargetValue = startOfPrefixMask;
+                //Starting value is set in base class in regards to algType
+                int targetValue = this->startingValue;
+                for (int i = startOfPrefixMask + maskOneHalfLength; i < signalSize ; i++) {
+                    int leftMostIndexOfMask = i - maskOneHalfLength;
+                    //Protects us against going out of bounds from left side
+                    if (leftMostIndexOfMask < 0) {
+                        leftMostIndexOfMask = 0;
+                    }
+                    //Protects us against going out of bounds from right side
+                    int rightMostIndexOfMask = i + maskOneHalfLength;
+                    if (rightMostIndexOfMask >= signalSize) {
+                        rightMostIndexOfMask = signalSize - 1;
+                    }
+                    //If index is outside of our window mask, we need to iterate through every number to find the MIN/MAX
+                    if (indexOfTargetValue < leftMostIndexOfMask || i == startOfPrefixMask + maskOneHalfLength) {
+                        targetValue = this->startingValue;
+                        for (int j = leftMostIndexOfMask; j <= rightMostIndexOfMask; j++) {
+                            if (this->compare(this->data[j], targetValue)) {
+                                targetValue = this->data[j];
+                                indexOfTargetValue = j;
+                            }
+                        }
+                        //If index is inside the mask, we need to check if the new value that entered the mask by moving it by one value
+                        //is more suitable than already found MIN/MAX value
+                    }
+                    else {
+                        if (this->compare(this->data[rightMostIndexOfMask], targetValue)) {
+                            targetValue = this->data[rightMostIndexOfMask];
+                            indexOfTargetValue = rightMostIndexOfMask;
+                        }
+                    }
+                    newPartOfSignal->pushBack(targetValue);
+                    std::cout << newPartOfSignal->getSignal().back() << " at index: " << i << std::endl;
+                }
+                break;
             }
             this->prePostFixes[endOfPrefixMask][endOfPrefixMask] = this->data[endOfPrefixMask];
             int lastBeforeLastPrefixMaskIndex = startOfPrefixMask + this->maskSize - 2; //coz we check j + 1
@@ -134,14 +172,85 @@ void SmartSignalFilter::filter(std::shared_ptr<Signal> newPartOfSignal, int firs
             //TODO: sprawdzic te przypadki kiedy sygnal nam sie konczy wczesniej niz te indeksy od masek
             int startOfPostfixMask = startOfPrefixMask + maskSize;
             if (startOfPostfixMask >= signalSize) {
-                startOfPostfixMask = signalSize - 1;
+                //stop
+                int indexOfTargetValue = startOfPrefixMask;
+                //Starting value is set in base class in regards to algType
+                int targetValue = this->startingValue;
+                for (int i = startOfPrefixMask + maskOneHalfLength; i < signalSize; i++) {
+                    int leftMostIndexOfMask = i - maskOneHalfLength;
+                    //Protects us against going out of bounds from left side
+                    if (leftMostIndexOfMask < 0) {
+                        leftMostIndexOfMask = 0;
+                    }
+                    //Protects us against going out of bounds from right side
+                    int rightMostIndexOfMask = i + maskOneHalfLength;
+                    if (rightMostIndexOfMask >= signalSize) {
+                        rightMostIndexOfMask = signalSize - 1;
+                    }
+                    //If index is outside of our window mask, we need to iterate through every number to find the MIN/MAX
+                    if (indexOfTargetValue < leftMostIndexOfMask || i == startOfPrefixMask + maskOneHalfLength) {
+                        targetValue = this->startingValue;
+                        for (int j = leftMostIndexOfMask; j <= rightMostIndexOfMask; j++) {
+                            if (this->compare(this->data[j], targetValue)) {
+                                targetValue = this->data[j];
+                                indexOfTargetValue = j;
+                            }
+                        }
+                        //If index is inside the mask, we need to check if the new value that entered the mask by moving it by one value
+                        //is more suitable than already found MIN/MAX value
+                    }
+                    else {
+                        if (this->compare(this->data[rightMostIndexOfMask], targetValue)) {
+                            targetValue = this->data[rightMostIndexOfMask];
+                            indexOfTargetValue = rightMostIndexOfMask;
+                        }
+                    }
+                    newPartOfSignal->pushBack(targetValue);
+                    std::cout << newPartOfSignal->getSignal().back() << " at index: " << i << std::endl;
+                }
+                break;
                 // it means that we can only calc one index, which is the middle index of previous mask
                 // thus we need to handle the indexes after the middle index of previous mask,
                 // probably the naive approach would be the best once again
             }
             int endOfPostfixMask = startOfPrefixMask + maskSize * 2 - 1;
             if (endOfPostfixMask >= signalSize) {
-                endOfPostfixMask = signalSize - 1;
+                int indexOfTargetValue = startOfPrefixMask;
+                //Starting value is set in base class in regards to algType
+                int targetValue = this->startingValue;
+                for (int i = startOfPrefixMask + maskOneHalfLength; i < signalSize; i++) {
+                    int leftMostIndexOfMask = i - maskOneHalfLength;
+                    //Protects us against going out of bounds from left side
+                    if (leftMostIndexOfMask < 0) {
+                        leftMostIndexOfMask = 0;
+                    }
+                    //Protects us against going out of bounds from right side
+                    int rightMostIndexOfMask = i + maskOneHalfLength;
+                    if (rightMostIndexOfMask >= signalSize) {
+                        rightMostIndexOfMask = signalSize - 1;
+                    }
+                    //If index is outside of our window mask, we need to iterate through every number to find the MIN/MAX
+                    if (indexOfTargetValue < leftMostIndexOfMask || i == startOfPrefixMask + maskOneHalfLength) {
+                        targetValue = this->startingValue;
+                        for (int j = leftMostIndexOfMask; j <= rightMostIndexOfMask; j++) {
+                            if (this->compare(this->data[j], targetValue)) {
+                                targetValue = this->data[j];
+                                indexOfTargetValue = j;
+                            }
+                        }
+                        //If index is inside the mask, we need to check if the new value that entered the mask by moving it by one value
+                        //is more suitable than already found MIN/MAX value
+                    }
+                    else {
+                        if (this->compare(this->data[rightMostIndexOfMask], targetValue)) {
+                            targetValue = this->data[rightMostIndexOfMask];
+                            indexOfTargetValue = rightMostIndexOfMask;
+                        }
+                    }
+                    newPartOfSignal->pushBack(targetValue);
+                    std::cout << newPartOfSignal->getSignal().back() << " at index: " << i << std::endl;
+                }
+                break;
                 // it means that we can calculate the indexes(by that i mean the middle indexes of masks) up to
                 // middle of prefix(postfix really) mask and up to some index which is defined by the last index of 
                 // endOfPostfixMask (not directly, it helps define it in some way)
@@ -193,87 +302,6 @@ void SmartSignalFilter::filter(std::shared_ptr<Signal> newPartOfSignal, int firs
                 std::cout << newPartOfSignal->getSignal().back() << " at index: " << j + maskOneHalfLength << std::endl;
             }
             std::cout << "End of iteration" << std::endl;
-        }
-
-        // now we can proceed to deal with the mid part of a signal
-        //TODO: wyjebac
-        for (int i = 1; i * this->maskSize * 2 < signalSize; i++) {
-            //prefixes
-            int startOfPrefixMask = i * this->maskSize * 2;
-            if (startOfPrefixMask >= signalSize) {
-                startOfPrefixMask = signalSize - 1;
-            }
-            int endOfPrefixMask = i * this->maskSize * 2 + this->maskSize - 1;
-            if (endOfPrefixMask >= signalSize) {
-                endOfPrefixMask = signalSize - 1;
-            }
-            this->prePostFixes[endOfPrefixMask][endOfPrefixMask] = this->data[endOfPrefixMask];
-            int lastBeforeLastPrefixMaskIndex = i * this->maskSize * 2 + this->maskSize - 2;
-            if (lastBeforeLastPrefixMaskIndex >= signalSize) {
-                lastBeforeLastPrefixMaskIndex = signalSize - 2; //coz we check j + 1
-            }
-            for (int j = lastBeforeLastPrefixMaskIndex; j >= startOfPrefixMask; j--) {
-                if (this->compare(this->data[j], this->prePostFixes[j + 1][endOfPrefixMask])) {
-                    this->prePostFixes[j][endOfPrefixMask] = this->data[j];
-                } else {
-                    this->prePostFixes[j][endOfPrefixMask] = this->prePostFixes[j + 1][endOfPrefixMask];
-                }
-            }
-            //postfixes
-            int startOfPostfixMask = i * this->maskSize * 2 + maskSize;
-            if (startOfPostfixMask >= signalSize) {
-                startOfPostfixMask = signalSize - 1;
-            }
-            int endOfPostfixMask = i * this->maskSize * 2 + maskSize * 2 - 1;
-            if (endOfPostfixMask >= signalSize) {
-                endOfPostfixMask = signalSize - 1;
-            }
-            this->prePostFixes[startOfPostfixMask][startOfPostfixMask] = this->data[startOfPostfixMask];
-            int nextAfterStartIndexOfPostfixMask = startOfPostfixMask + 1;
-            if (nextAfterStartIndexOfPostfixMask >= signalSize) {
-                nextAfterStartIndexOfPostfixMask = signalSize - 1;
-            }
-            if (nextAfterStartIndexOfPostfixMask < signalSize) {
-                for (int j = nextAfterStartIndexOfPostfixMask; j < startOfPostfixMask + this->maskSize; j++) {
-                    if (this->compare(this->data[j], this->prePostFixes[startOfPostfixMask][j - 1])) {
-                        this->prePostFixes[startOfPostfixMask][j] = this->data[j];
-                    }
-                    else {
-                        this->prePostFixes[startOfPostfixMask][j] = this->prePostFixes[startOfPostfixMask][j - 1];
-                    }
-                }
-            }
-            //
-            for (int j = startOfPrefixMask; j <= startOfPostfixMask; j++) {
-                int leftMostIndexOfMask = j - maskOneHalfLength;
-                //Protects us against going out of bounds from left side
-                if (leftMostIndexOfMask < 0) {
-                    leftMostIndexOfMask = 0;
-                }
-                //I think that might not be needed since we restrict it in j loop values
-                int rightMostIndexOfMask = j + maskOneHalfLength;
-                if (rightMostIndexOfMask > endOfPostfixMask) {
-                    rightMostIndexOfMask = endOfPostfixMask;
-                }
-                
-                //if right index is in postfix part, we need to properly use prefix and postfix part
-                if (j == startOfPostfixMask) {
-                    if (this->compare(this->prePostFixes[leftMostIndexOfMask][j-1], this->prePostFixes[j][rightMostIndexOfMask])) {
-                        newPartOfSignal->pushBack(this->prePostFixes[leftMostIndexOfMask][j - 1]);
-                    } else {
-                        newPartOfSignal->pushBack(this->prePostFixes[j][rightMostIndexOfMask]);
-                    }
-                } else if (rightMostIndexOfMask >= startOfPostfixMask) {
-                    if (this->compare(this->prePostFixes[leftMostIndexOfMask][endOfPrefixMask], this->prePostFixes[startOfPostfixMask][rightMostIndexOfMask])) {
-                        newPartOfSignal->pushBack(this->prePostFixes[leftMostIndexOfMask][endOfPrefixMask]);
-                    } else {
-                        newPartOfSignal->pushBack(this->prePostFixes[startOfPostfixMask][rightMostIndexOfMask]);
-                    }
-                } else {
-                    newPartOfSignal->pushBack(this->prePostFixes[rightMostIndexOfMask][leftMostIndexOfMask]);
-                }
-                std::cout << newPartOfSignal->getSignal().back() << std::endl;
-            } 
         }
     }
 }
